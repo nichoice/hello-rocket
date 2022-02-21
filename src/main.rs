192d3149ce5d654d58fbae72d7d67b26e7eb214e
nichoice <798254259@qq.com>
2022-02-21 23:24:17 +0800

    feat: restful demo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // use rocket::{get, launch, routes};
-use rocket::{get, routes};
+use rocket::{get, post, put, delete, routes};
 
 
 #[get("/")]
 async fn hello() -> String {
     "hello world!".to_string()
 }
+
 
 // offical launch method
 
@@ -15,10 +16,40 @@ async fn hello() -> String {
 //         .mount("/hello", routes![hello])
 // }
 
+
+// restful
+// get method
+
+#[get("/test")]
+async fn test_get() -> String {
+    "test get method".to_string()
+}
+
+// post method
+#[post("/test")]
+async fn test_post() -> String {
+    "test post method".to_string()
+}
+
+// _ 消除告警
+//put method
+#[put("/test/<_id>")]
+async fn test_put(_id: usize) -> String {
+    "test put method".to_string()
+}
+
+//delete method
+#[delete("/test/<_id>")]
+async fn test_delete(_id: usize) -> String {
+    "test delete method".to_string()
+}
+
+
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     rocket::build()
         .mount("/hello", routes![hello])
+        .mount("/test", routes![test_get, test_post, test_put, test_delete])
         .launch().await?;
     Ok(())
 }
