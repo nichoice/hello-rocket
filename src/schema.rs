c5493a92a868d46577d844949de7e6d50e642bf8
Nicholas <798254259@qq.com>
2022-03-01 14:00:52 +0800

    feat: database table schema

---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+table! {
+    users (id) {
+        id -> Nullable<Integer>,
+        username -> Text,
+        email -> Text,
+        password -> Text,
+        // created_at -> Timestamp,
+        // updated_at -> Timestamp,
+    }
+}
